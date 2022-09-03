cc5c38f8288d8f84e256edade9bea23523d248d2
DeeprajB <deeprajb012@gmail.com>
2022-09-03 22:06:47 +0530

    Initialised Quilljs

--- notebuddy-client/src/App.js
+++ notebuddy-client/src/App.js
@@ -1,26 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 import { ChakraProvider } from '@chakra-ui/react'
+
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 function App() {
   return (
     <ChakraProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ReactQuill theme="snow"/>
     </div>
     </ChakraProvider>
   );
