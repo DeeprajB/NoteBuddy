f065e6edc34af2021617de6c1f92e87e6901f2d8
DeeprajB <deeprajb012@gmail.com>
2022-09-04 07:53:24 +0530

    Added Components and Pages Directory.

--- notebuddy-client/src/App.js
+++ notebuddy-client/src/App.js
@@ -1,16 +1,11 @@
-import './App.css';
-
 import { ChakraProvider } from '@chakra-ui/react'
 
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+import Home from './pages/Home'
 
 function App() {
   return (
     <ChakraProvider>
-    <div className="App">
-    <ReactQuill theme="snow"/>
-    </div>
+    <Home />
     </ChakraProvider>
   );
 }
